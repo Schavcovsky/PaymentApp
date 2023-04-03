b7017581c77898c01ea2ad867107bc11d68cce11
Alejandro Villalobos <alejandro.ignacio.villalobos@gmail.cl>
2023-04-02 23:02:46 -0400

    Cleaning up

--- Payments/Services/PaymentsMethodsInstallmentsService.swift
+++ Payments/Services/PaymentsMethodsInstallmentsService.swift
@@ -14,20 +14,15 @@ protocol PaymentsMethodsInstallmentsServiceProtocol {
 
 class PaymentsMethodsInstallmentsService: NetworkManager, PaymentsMethodsInstallmentsServiceProtocol {
     func getPaymentMethodsInstallments(userSelection: UserSelection, completion: @escaping (Result<[Installments], Error>) -> Void) {
-        guard let publicKey = PlistHelper.value(forKey: "publicKey",
-                                                fromPlist: "Environment"),
+        guard let publicKey = PlistHelper.value(forKey: "publicKey", fromPlist: "Environment")?.trimmingCharacters(in: .whitespacesAndNewlines),
               let selectedAmount = userSelection.current.amount,
-              let selectedPaymentMethodId = userSelection.current.paymentMethodId,
-              let selectedBank = userSelection.current.bankId
+              let selectedPaymentMethodId = userSelection.current.paymentMethodId?.trimmingCharacters(in: .whitespacesAndNewlines),
+              let selectedBank = userSelection.current.bankId?.trimmingCharacters(in: .whitespacesAndNewlines)
         else {
             return
         }
         
-        let cleanedPublicKey = publicKey.trimmingCharacters(in: .whitespacesAndNewlines)
-        let cleanedSelectedPaymentMethodId = selectedPaymentMethodId.trimmingCharacters(in: .whitespacesAndNewlines)
-        let cleanedSelectedPaymentBank = selectedBank.trimmingCharacters(in: .whitespacesAndNewlines)
-        
-        let urlString = "https://api.mercadopago.com/v1/payment_methods/installments?public_key=\(cleanedPublicKey)&amount=\(selectedAmount)&payment_method_id=\(cleanedSelectedPaymentMethodId)&issuer.id=\(cleanedSelectedPaymentBank)"
+        let urlString = "https://api.mercadopago.com/v1/payment_methods/installments?public_key=\(publicKey)&amount=\(selectedAmount)&payment_method_id=\(selectedPaymentMethodId)&issuer.id=\(selectedBank)"
         
         NetworkManager.request(url: urlString, httpMethod: .get, expecting: [Installments].self) { result, _, _, _ in
             completion(result)
